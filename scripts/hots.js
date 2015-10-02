181025af3d81468c4f88e9ffc41bc5d9fe656c18
Bryan <bryan792@gmail.com>
2015-10-02 13:37:11 -0700

    First brybot commit before passing to sam

---
+++ scripts/hots.js
@@ -0,0 +1,147 @@
+// Description:
+//   When asked, tells users about the current Heroes of the Storm rotation.
+//   Will eventually learn to tell users about their MMR and other data tracked
+//   at hotslogs.com.
+//
+// Dependencies:
+//   "cheerio": "^0.19.0"
+//   "request": "^2.55.0"
+//   "lodash": "^3.9.3"
+//
+// Commands:
+//   hubot hots rotation - fetches the current rotation
+//   hubot hots mmr @<username> - fetches the MMR of the given player
+//   hubot hots mmr @<autocompleted discord name> - fetches the MMR of the given player
+//   hubot hots mmr <hotslog id> - fetches the MMR of the given id
+//   hubot hots mmr - fetches the MMR of the requesting user, given they've registered it
+//   hubot hots register <id> - remembers the requesting user's hotslogs ID
+//
+// Author:
+//   bryan792
+//   frio
+
+var _, cheerio, request, rp;
+cheerio = require('cheerio');
+request = require('request');
+_ = require('lodash');
+rp = require('request-promise');
+
+module.exports = function(robot) {
+
+  var fetchMMR = function(id, robotResponse) {
+    console.log(id);
+    rp("https://www.hotslogs.com/API/Players/" + id).then(function(body) {
+      return console.log(body);
+    });
+    return request("https://www.hotslogs.com/API/Players/" + id, function(err, httpResponse, body) {
+      var mmrs, name, stats;
+      if (err) {
+        robot.logger.error(err);
+        robotResponse.send("I'm sorry, something went wrong :(");
+        return;
+      }
+      stats = JSON.parse(body);
+      name = stats.Name;
+      mmrs = _.map(stats.LeaderboardRankings, function(val) {
+        return val.CurrentMMR + " (" + val.GameMode + ")";
+      });
+      return robotResponse.send(name + " -- " + (mmrs.join(', ')));
+    });
+  };
+
+  robot.respond(/hots rotation/i, function(robotResponse) {
+    return request('http://heroesofthestorm.github.io/free-hero-rotation', function(err, httpResponse, body) {
+        var $, heroes, validFor;
+        if (err) {
+          robot.logger.error(err);
+          robotResponse.send("I'm sorry, something went wrong :(");
+          return;
+        }
+        $ = cheerio.load(body);
+        validFor = $('h2').first().text();
+        heroes = $('button.btn:not(.dropdown-toggle)').map(function(i, e) {
+          return e.children[0].data;
+        }).slice(0, 7).get();
+        return robotResponse.send(validFor + ": " + (heroes.join(', ')));
+        });
+  });
+
+  robot.respond(/hots mmr( \@[\w\d]+)?$/i, function(robotResponse) {
+    var ref, user, username;
+    username = (ref = robotResponse.match[1]) != null ? ref.trim().slice(1) : void 0;
+    if (username) {
+      user = robot.brain.usersForFuzzyName(username)[0];
+    } else {
+      user = robot.brain.userForId(robotResponse.message.user.id);
+      username = user.name;
+    }
+    if (user == null) {
+      robotResponse.reply("Sorry, I don\'t know who " + username + " is :(");
+      return;
+    }
+    if (user.hotsLogsId != null) {
+      return fetchMMR(user.hotsLogsId, robotResponse);
+    } else if (username != null) {
+      return robotResponse.reply("Sorry, " + username + " hasn't registered their hotslogs ID.  Bully them until they do.");
+    } else {
+      return robotResponse.reply('Sorry, your ID isn\'t registered. Please register your ID with `hots register <id>`, where your ID is the numeric ID for your hotslogs profile.');
+    }
+  });
+
+  robot.respond(/hots mmr <(\@[\d]+)?>$/i, function(robotResponse) {
+    var ref, user, username;
+    username = ((ref = robotResponse.match[1]) != null ? ref.trim().slice(1) : void 0) || robotResponse.message.user.name;
+    user = robot.brain.userForId(username);
+    if (user == null) {
+      robotResponse.reply("Sorry, I don\'t know who " + username + " is :(");
+      return;
+    }
+    if (user.hotsLogsId != null) {
+      return fetchMMR(user.hotsLogsId, robotResponse);
+    } else if (username != null) {
+      return robotResponse.reply("Sorry, " + username + " hasn't registered their hotslogs ID.  Bully them until they do.");
+    } else {
+      return robotResponse.reply('Sorry, your ID isn\'t registered. Please register your ID with `hots register <id>`, where your ID is the numeric ID for your hotslogs profile.');
+    }
+  });
+
+  robot.respond(/hots mmr (\d+)/i, function(robotResponse) {
+    var hotsLogsId;
+    hotsLogsId = robotResponse.match[1];
+    return fetchMMR(hotsLogsId, robotResponse);
+  });
+
+  robot.respond(/hots all mmr/i, function(robotResponse) {
+    var key, ref, results, user;
+    ref = robot.brain.data.users;
+    results = [];
+    for (key in ref) {
+      user = ref[key];
+      if (user.hotsLogsId) {
+        results.push(fetchMMR(user.hotsLogsId, robotResponse));
+      } else {
+        results.push(void 0);
+      }
+    }
+    return results;
+  });
+
+  robot.respond(/hots register (\d+)/i, function(robotResponse) {
+    var hotsLogsId, user;
+    hotsLogsId = robotResponse.match[1];
+    user = robot.brain.userForId(robotResponse.message.user.id, {
+      name: robotResponse.message.user.name
+    });
+    user.hotsLogsId = hotsLogsId;
+    return robotResponse.reply("assigned " + hotsLogsId + " as your hotslogs id");
+  });
+
+  robot.respond(/hots register <(\@[\d]+)?> (\d+)$/i, function(robotResponse) {
+    var hotsLogsId, ref, user, username;
+    username = (ref = robotResponse.match[1]) != null ? ref.trim().slice(1) : void 0;
+    hotsLogsId = robotResponse.match[2];
+    user = robot.brain.userForId(username);
+    user.hotsLogsId = hotsLogsId;
+    return robotResponse.reply("assigned " + hotsLogsId + " as " + user.name + " hotslogs id");
+  });
+};
---
+++ scripts/mopidy.js
@@ -0,0 +1,170 @@
+var Mopidy, mopidy, online;
+
+Mopidy = require("mopidy");
+
+mopidy = new Mopidy({
+  webSocketUrl: 'ws://localhost:6680/mopidy/ws/'
+});
+
+online = false;
+
+mopidy.on('state:online', function() {
+  return online = true;
+});
+
+mopidy.on('state:offline', function() {
+  return online = false;
+});
+
+module.exports = function(robot) {
+
+  mopidy.on("event:trackPlaybackStarted", function() {
+    mopidy.playback.getCurrentTrack()
+      .done(function(track) {
+        if (track) {
+          console.log(track);
+          robot.adapter.topic(null, "Currently playing: " + constructTrackDesc(track));
+        }
+      });
+  });
+
+  robot.respond(/set volume (\d+)/i, function(message) {
+    var newVolume;
+    newVolume = parseInt(message.match[1]);
+    if (online) {
+      mopidy.playback.setVolume(newVolume);
+      return message.send("Set volume to " + newVolume);
+    } else {
+      return message.send('Mopidy is offline');
+    }
+  });
+
+  robot.respond(/volume\?/i, function(message) {
+    var printCurrentVolume;
+    if (online) {
+      printCurrentVolume = function(volume) {
+        if (volume) {
+          return message.send("The Current volume is " + volume);
+        } else {
+          return message.send("Sorry, can't grab current volume");
+        }
+      };
+    } else {
+      message.send('Mopidy is offline');
+    }
+    return mopidy.playback.getVolume().then(printCurrentVolume, console.error.bind(console));
+  });
+
+  robot.respond(/what'?s playing/i, function(message) {
+    var printCurrentTrack;
+    if (online) {
+      printCurrentTrack = function(track) {
+        if (track) {
+          return message.send("Currently playing: " + constructTrackDesc(track));
+        } else {
+          return message.send("No track is playing");
+        }
+      };
+    } else {
+      message.send('Mopidy is offline');
+    }
+    return mopidy.playback.getCurrentTrack().done(printCurrentTrack, console.error.bind(console));
+  });
+
+  robot.respond(/next track/i, function(message) {
+    var printCurrentTrack;
+    if (online) {
+      mopidy.playback.next();
+      printCurrentTrack = function(track) {
+        if (track) {
+          return message.send("Now playing: " + track.name + " by " + track.artists[0].name + " from " + track.album.name);
+        } else {
+          return message.send("No track is playing");
+        }
+      };
+    } else {
+      message.send('Mopidy is offline');
+    }
+    return mopidy.playback.getCurrentTrack().then(printCurrentTrack, console.error.bind(console));
+  });
+
+  robot.respond(/mute/i, function(message) {
+    if (online) {
+      mopidy.playback.setMute(true);
+      return message.send('Playback muted');
+    } else {
+      return message.send('Mopidy is offline');
+    }
+  });
+
+  robot.respond(/unmute/i, function(message) {
+    if (online) {
+      mopidy.playback.setMute(false);
+      return message.send('Playback unmuted');
+    } else {
+      return message.send('Mopidy is offline');
+    }
+  });
+
+  robot.respond(/pause music/i, function(message) {
+    if (online) {
+      mopidy.playback.pause();
+      return message.send('Music paused');
+    } else {
+      return message.send('Mopidy is offline');
+    }
+  });
+
+  robot.respond(/resume music/i, function(message) {
+    if (online) {
+      mopidy.playback.resume();
+      return message.send('Music resumed');
+    } else {
+      return message.send('Mopidy is offline');
+    }
+  });
+
+  robot.respond(/shuffle music/i, function(message) {
+    if (online) {
+      mopidy.tracklist.setRandom(true);
+      return message.send('Now shuffling');
+    } else {
+      return message.send('Mopidy is offline');
+    }
+  });
+
+  robot.respond(/stop shuffle/i, function(message) {
+    if (online) {
+      mopidy.tracklist.setRandom(false);
+      return message.send('Shuffling has been stopped');
+    } else {
+      return message.send('Mopidy is offline');
+    }
+  });
+
+  robot.respond(/search (.*)/i, function(message) {
+    if (online) {
+      var query = message.match[1];
+      mopidy.library.search({
+        'any': [query]
+      }).then(function(data) {
+        var track = data[0].tracks[0];
+        console.log(data);
+        mopidy.tracklist.add([track]).then(function(data) {
+          console.log(data);
+          mopidy.playback.play(data[0]);
+        });
+        return message.send("https://www.youtube.com/watch?v=" + data[0].tracks[0].comment);
+      });
+    } else {
+      return message.send('Mopidy is offline');
+    }
+  });
+
+  var constructTrackDesc = function(track) {
+    var desc = "";
+    if (track.name) desc += track.name;
+    if (track.artists && track.artists[0].name) desc += " by " + track.artists[0].name;
+    return desc;
+  }
+};
